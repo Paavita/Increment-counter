32b3efc699192ab1debc2c37ed56c558a29ca59d
Paavita <129326430+Paavita@users.noreply.github.com>
2023-03-30 01:01:01 +0530

    Add files via upload

---
+++ increment-counter/script.js
@@ -0,0 +1,14 @@
+let countEl = document.getElementById("count-el")
+let count = 0
+let saveEl = document.getElementById('save-el')
+function increment(){
+  count += 1
+  countEl.textContent = count
+}
+
+function save(){
+  let entries = count + " - "
+  saveEl.textContent += entries
+  countEl.textContent = 0
+  count = 0
+}
